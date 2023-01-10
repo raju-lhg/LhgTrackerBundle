77d254edc189c400c2a68281dea698289fb2828f
Raju at LHG <raju@lhgraphics.com>
2023-01-11 04:10:59 +0600

    Added controller

---
+++ Controller/DemoController.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the DemoBundle for Kimai 2.
+ * All rights reserved by Kevin Papst (www.kevinpapst.de).
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KimaiPlugin\LhgTrackerBundle\Controller;
+
+use App\Controller\AbstractController; 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route(path="/admin/lhg-tracker")
+ * @Security("is_granted('demo')")
+ */
+// final class LhgTrackerController extends AbstractController
+final class LhgTrackerController
+{
+   
+}
