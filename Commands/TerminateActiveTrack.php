a1825dff2ee116699c902e5d5a2e1c53fc0d7005
Raju at LHG <raju@lhgraphics.com>
2023-01-11 02:05:44 +0600

    Pulled active records

--- Commands/TerminateActiveTrack.php
+++ Commands/TerminateActiveTrack.php
@@ -2,18 +2,31 @@
 
 namespace KimaiPlugin\LhgTrackerBundle\Commands;
 
+use App\Repository\TimesheetRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use App\Utils\LocaleFormatter;
+use Psr\Log\LoggerInterface;
 
 class TerminateActiveTrack extends Command
 {
     protected static $defaultDescription = 'Terminates all active records that exceed allocated budget.';
     protected static $defaultName = 'lhg-tracker:auto-terminate';
     protected $io;
+    private $time_sheet_repository;
+    private $logger;
+
+    public function __construct( 
+        TimesheetRepository $time_sheet_repository,
+        LoggerInterface $logger
+    ) {
+        parent::__construct();
+        $this->time_sheet_repository = $time_sheet_repository;
+        $this->logger = $logger;
+    }
     
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -26,5 +39,12 @@ class TerminateActiveTrack extends Command
 
     private function terminate_active_records(){
         $this->io->writeln("Terminating all active Tracks ...");
+        $activeEntries = $this->time_sheet_repository->getActiveEntries();
+        foreach ($activeEntries as $key => $timesheet) {
+            $this->logger->info([
+                $timesheet->getId() => $timesheet
+            ]);
+        }
+        
     }
 }
