08c5b6d15797801fbf2405d5b7d5fdc4a0116c49
Raju Rayhan <raju.rayhan@yandex.com>
2023-01-16 19:14:04 +0600

    Added logs for budget entry

--- Commands/TerminateActiveTrack.php
+++ Commands/TerminateActiveTrack.php
@@ -66,6 +66,7 @@ class TerminateActiveTrack extends Command
 
             $query = new ProjectQuery();  
             $budgetData     = $this->budgetRepository->getBudgetDataForProjectList($query);
+            $this->io->writeln(json_encode($budgetData)); 
             $projectIds     = \array_column($budgetData, 'id');
             $projectBudgets = [];
             $projects       = [];
