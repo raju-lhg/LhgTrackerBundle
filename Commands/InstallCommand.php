6eae0b3945dda0e18aefe4a581d92cf99f4e368c
Raju at LHG <raju@lhgraphics.com>
2023-01-22 01:44:18 +0600

    Inserted new row for project budget

--- Commands/InstallCommand.php
+++ Commands/InstallCommand.php
@@ -2,6 +2,10 @@
 
 namespace KimaiPlugin\LhgTrackerBundle\Commands;
 
+use App\Entity\EntityWithMetaFields;
+use App\Entity\MetaTableTypeInterface;
+use App\Entity\Project;
+use App\Entity\ProjectMeta;
 use App\Repository\Query\ProjectQuery;
 use App\Repository\TimesheetRepository;
 use App\Timesheet\TimesheetService;
@@ -9,12 +13,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use App\Utils\LocaleFormatter;
+use Symfony\Component\Console\Style\SymfonyStyle; 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
+use KimaiPlugin\MetaFieldsBundle\Entity\MetaFieldRule;
+use KimaiPlugin\MetaFieldsBundle\Repository\MetaFieldRuleRepository;
 use KimaiPlugin\RecurringBudgetBundle\EventSubscriber\ProjectSubscriber;
 use KimaiPlugin\RecurringBudgetBundle\Repository\BudgetRepository;
 use Psr\Log\LoggerInterface;
 use KimaiPlugin\RecurringBudgetBundle\Utils\Utils;
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Component\Serializer\Encoder\JsonEncode;
 
 class InstallCommand extends Command
@@ -25,19 +33,25 @@ class InstallCommand extends Command
     private $time_sheet_repository;
     private $budgetRepository;
     private $time_sheet_service;
-    private $logger;
+    private $logger; 
+    private $entityManager;
+    // private $metaFieldRuleRepository;
 
     public function __construct( 
         TimesheetRepository $time_sheet_repository,
         BudgetRepository $budgetRepository,
         TimesheetService $time_sheet_service,
-        LoggerInterface $logger
+        LoggerInterface $logger, 
+        EntityManagerInterface $entityManager,
+        // MetaFieldRuleRepository $metaFieldRuleRepository
     ) {
         parent::__construct();
         $this->time_sheet_repository = $time_sheet_repository;
         $this->budgetRepository = $budgetRepository;
         $this->time_sheet_service = $time_sheet_service;
-        $this->logger = $logger;
+        $this->logger = $logger; 
+        $this->entityManager = $entityManager;
+        // $this->metaFieldRuleRepository = $metaFieldRuleRepository;
     }
     
 
@@ -45,109 +59,59 @@ class InstallCommand extends Command
     {
         $this->io    = new SymfonyStyle($input, $output);
         $this->io->writeln("<bg=green>********************************** Installing LhgTrackerBundle **********************************************</>");
-        $this->io->writeln("<bg=green>============================================================================================================</>");
-        // $this->terminate_active_records();
+        $this->io->writeln("<bg=green>=============================================================================================================</>");
+        if($this->check_if_custom_field_exists()){
+                $this->io->writeln(" *************** Meta field already exists! ***************"); 
+        }
+        else{
+            $this->add_custom_field();
+        }
+        $this->io->writeln("<bg=green>**************************** Installed LhgTrackerBundle Successfully! ****************************************</>");
         return 0;
     }
 
-    private function terminate_active_records(){
+    private function check_if_custom_field_exists(): bool{
+
         try {
-            $this->io->writeln("Terminating all active Tracks ...");
-            $activeEntries = $this->time_sheet_repository->getActiveEntries();
-            $activeProjectIds = [];
-            foreach ($activeEntries as $key =>  $timeEntry) {
-                array_push($activeProjectIds, $timeEntry->getProject()->getId());
+            $this->io->writeln(" *************** Checking for Metafields ***************"); 
+            $metaFieldEntity = new MetaFieldRule(); 
+
+            $existingField = $this->entityManager->getRepository(get_class($metaFieldEntity))->findOneBy([
+                "name" => "allow_over_budget_on_project"
+            ]);
+            if($existingField){
+                return true;
             }
-            $this->io->writeln(json_encode($activeProjectIds)); 
-            $index = 0;
-            foreach ($activeEntries as $key => $timesheet) {
-                $index++;
-                $consoleOutput = $timesheet->getProject()->getId()."# <info>".$timesheet->getUser()->getDisplayName() ." => " . $timesheet->getProject()->getName(). " => " .  $timesheet->getDescription()."</info>";
-                $this->io->writeln($consoleOutput); 
-
-                $query = new ProjectQuery();  
-                $budgetData     = $this->budgetRepository->getBudgetDataForProjectList($query); 
-                $projectIds     = \array_column($budgetData, 'id');
-                $projectBudgets = [];
-                $projects       = [];
-
-                foreach ($budgetData as $entry) {
-                    // $this->io->writeln(json_encode($entry));
-                    if(in_array($entry['id'], $activeProjectIds)){
-                        $project = $timesheet->getProject();
-
-                        if (empty($project)) {
-                            continue;
-                        }
-
-                        $budgetType           = Utils::getProjectBudgetType($project);
-                        $hasRecurringBudget   = true;
-                        $budgetRecurringValue = null;
-
-                        if (!$budgetType) {
-                            $hasRecurringBudget = false;
-
-                            if ($project->getBudget() > 0) {
-                                $budgetType = ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_MONEY;
-                            } elseif ($project->getTimeBudget() > 0) {
-                                $budgetType = ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_TIME;
-                            } elseif ((int)$entry['time_budget_left'] !== 0) {
-                                $budgetType = ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_TIME;
-                            } elseif ((int)$entry['budget_left'] !== 0) {
-                                $budgetType = ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_MONEY;
-                            }
-                        } else {
-                            switch ($budgetType) {
-                                case ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_TIME:
-                                    $budgetRecurringValue = $project
-                                        ->getMetaField(ProjectSubscriber::RECURRING_TIME_BUDGET_META_FIELD)
-                                        ->getValue();
-
-                                    $budgetRecurringValue = Utils::convertDurationStringToSeconds($budgetRecurringValue);
-                                    break;
-
-                                case ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_MONEY:
-                                    $budgetRecurringValue = $project
-                                        ->getMetaField(ProjectSubscriber::RECURRING_MONEY_BUDGET_META_FIELD)
-                                        ->getValue();
-                                    break;
-                            }
-                        }
-
-                        if (\is_null($entry['time_budget_left']) || \is_null($entry['budget_left'])) {
-                            switch ($budgetType) {
-                                case ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_TIME:
-                                    $entry['time_budget_left'] = $project->getTimeBudget();
-                                    break;
-
-                                case ProjectSubscriber::PROJECT_RECURRING_BUDGET_TYPE_MONEY:
-                                    $entry['budget'] = $project->getBudget();
-                                    break;
-                            }
-                        }
-
-                        $entry['budgetRecurringValue'] = $budgetRecurringValue;
-                        $entry['budgetType']           = $budgetType;
-                        $entry['hasRecurringBudget']   = $hasRecurringBudget;
-
-                        $projectBudgets[$entry['id']] = $entry;
-                        $projects[]                   = $project;
-                        // Terminate The Tracker Record
-                        if((int) $entry['time_budget_left'] <=0 || (int) $entry['budget_left'] <=0){
-                            // $this->time_sheet_service->stopTimesheet($timesheet); 
-                        }
-                        $this->io->writeln("Following Entry Stopped"); 
-                        $this->io->writeln(json_encode($entry)); 
-                    }
-                    else{
-                        // $this->io->writeln("No active record for this project");
-                    }
-                }
-                
-            }
+            return false;
         } catch (\Throwable $th) {
             $this->io->writeln($th->getMessage()); 
         }
         
+    }
+
+    private function add_custom_field(): void{
+        try {
+            $this->io->writeln("********************************** Adding custom field **********************************************"); 
+            $entity = $this->prepareEntity();
+            $this->entityManager->persist($entity);
+            $this->entityManager->flush();
+            
+        } catch (\Throwable $th) {
+            $this->io->writeln($th->getMessage()); 
+        }
+        
+    }
+    private function prepareEntity() :MetaFieldRule
+    {
+        $entity = new MetaFieldRule();
+        $entity->setName('allow_over_budget_on_project');
+        $entity->setEntityType(get_class(new Project()));
+        $entity->setValue(0);
+        $entity->setVisible(1); 
+        $entity->setType('boolean');
+        $entity->setLabel('Allow Tracking Over Budget');
+        $entity->setHelp('Select Yes if you want to allow.');
+
+        return $entity;
     }
 }
