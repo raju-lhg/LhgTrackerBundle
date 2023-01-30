fcfe85dc015e1d2ef0e7dee260fabd7199e82ecc
Raju Rayhan <raju.rayhan@yandex.com>
2023-01-30 15:54:11 +0600

    Menu Subs On Progress

---
+++ EventSubscriber/MenuSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the CustomCSSBundle.
+ * All rights reserved by Kevin Papst (www.kevinpapst.de).
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KimaiPlugin\LhgTrackerBundle\EventSubscriber;
+
+use App\Event\ConfigureMainMenuEvent;
+use App\Utils\MenuItemModel;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class MenuSubscriber implements EventSubscriberInterface
+{
+    private $security;
+
+    public function __construct(AuthorizationCheckerInterface $security)
+    {
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConfigureMainMenuEvent::class => ['onMenuConfigure', 100],
+        ];
+    }
+
+    public function onMenuConfigure(ConfigureMainMenuEvent $event): void
+    {
+        $auth = $this->security;
+
+        // if (!$auth->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+        //     return;
+        // }
+
+        $menu = $event->getSystemMenu();
+
+        if ($auth->isGranted('active_now_lhg_tracker')) {
+            $menu->addChild(
+                new MenuItemModel('lhg-tracker', 'Active Now', 'lhg-tracker', [], 'as fa-clock') 
+            );
+        }
+    }
+}
