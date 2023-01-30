09d2a2dc2ec65f771047ea290a265c341448c796
Raju Rayhan <raju.rayhan@yandex.com>
2023-01-30 14:35:33 +0600

    Registered permission

--- DependencyInjection/LhgTrackerExtension.php
+++ DependencyInjection/LhgTrackerExtension.php
@@ -1,36 +1,47 @@
-<?php 
+<?php
+
+/*
+ * This file is part of the LhgTrackerBundle.
+ * All rights reserved by Kevin Papst (www.kevinpapst.de).
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace KimaiPlugin\LhgTrackerBundle\DependencyInjection;
 
-use Doctrine\ORM\Query\Parser;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class LhgTrackerExtension extends Extension
+class LhgTrackerExtension extends Extension implements PrependExtensionInterface
 {
-    public function load(array $configs, ContainerBuilder $container)
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader(
-            $container, 
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
-
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
-        // $loader->load('twig.yaml');
     }
 
     public function prepend(ContainerBuilder $container): void
     {
-        $yamlParser = new Parser();
-
-        // load the entity serialization config (mainly for the API)
-        $serializerConfig = file_get_contents(__DIR__ . '/../Resources/config/jms_serializer.yaml');
-        if ($serializerConfig === false) {
-            throw new \Exception('Could not read serializer configuration');
-        }
-        $config = $yamlParser->parse($serializerConfig);
-        $container->prependExtensionConfig('jms_serializer', $config['jms_serializer']); 
+        /*
+         * @CloudRequired adapt if new permissions are added
+         */
+        $container->prependExtensionConfig('kimai', [
+            'permissions' => [
+                'roles' => [
+                    'ROLE_SUPER_ADMIN' => [
+                        'active_now_lhg_tracker'
+                    ],
+                ],
+            ],
+        ]);
     }
 }
